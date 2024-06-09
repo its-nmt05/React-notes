25f187fe8a6acf67c9bc1c12549645d4e2d571bd
Nirmalya <nirmalyamallickthakur@gmail.com>
2024-06-09 19:29:57 +0530

    Learnt about React objects

--- 01_vite_react/src/App.jsx
+++ 01_vite_react/src/App.jsx
@@ -1,12 +1,20 @@
 import Chai from "./Chai.jsx"
 
 function App() {
+  const username = "Nirmalya"
+
   return (
     <>
       <Chai />
+      <h1>Chai aur React | {username}</h1>  
       <p>test para</p>
     </>
   )
 }
+
+/*
+In <h1>Chai aur React | {username}</h1>, {username}, is not an expression, instead
+it is an evaluated expression. This means 
+*/
 
 export default App
--- 01_vite_react/src/main.jsx
+++ 01_vite_react/src/main.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.jsx"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App</h1>
+    </div>
+  )
+}
+
+/*
+After parsing by something like Babel, the React componets get
+ultimately converted to React Objects
+*/
+// const ReactElement = {
+//   type: "a",
+//   props: {
+//       href: "https://www.google.com",
+//       target: "_blank",
+//   },
+//   children: "Click me to visit Google",
+// }
+
+const anotherElement = (
+  <a href="https://www.google.com" target="_blank">
+    Visit Google
+  </a>
+)
+
+const reactElement = React.createElement(
+  "a",
+  {
+    href: "https://www.google.com",
+    target: "_blank",
+  },
+  "click me to visit Google",
+)
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  /*
+      The reason why this wouldn't work is that, 
+      the ReactElement is a custom implementation of React objects
+      and can only be understood by a custom renderer builty specifically
+      for it. 
+      
+      The render() function provided by React will not be able
+      to understand the meaning(names) of all the different keys 
+      in our custom implementation. Since it was written for React objects
+      and not for our custom implementation of React objects.
+
+      For e.g => We've used the key 'type' to refer to the tag name of the element,
+      but in React's own implementation, that could have been 'tagName', etc.
+      So the render() function would never be able to understand this.
+      There is a particular type of syntax to the parameters the render() function expects.
+    */
+  // ReactElement
+
+  // this will be parsed correctly
+  //anotherElement
+
+  <App />
+  // MyApp() # will work but not recommended
 )
