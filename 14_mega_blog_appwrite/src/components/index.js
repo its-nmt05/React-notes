6f63d583987420c691089015c7fbd17d0063f407
Nirmalya <nirmalyamallickthakur@gmail.com>
2024-06-16 12:51:55 +0530

    Mega blog app using Appwrite

---
+++ 14_mega_blog_appwrite/src/components/index.js
@@ -0,0 +1,43 @@
+import Header from "./Header/Header"
+import Footer from "./Footer/Footer"
+import Container from "./container/Container"
+import Logo from "./Logo"
+import LogoutBtn from "./Header/LogoutBtn"
+import Input from "./Input"
+import Select from "./Select"
+import RTE from "./RTE"
+import PostForm from "./post-form/PostForm"
+import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout"
+import Button from "./Header/Button"
+
+import Login from "./Login"
+import Signup from "./Signup"
+import Home from "../pages/Home"
+import Post from "../pages/Post"
+import EditPost from "../pages/EditPost"
+import AddPost from "../pages/AddPost"
+import AllPosts from "../pages/AllPosts"
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Input,
+    Select,
+    RTE,
+    PostForm,
+    PostCard,
+    AuthLayout,
+    Button,
+    // pages
+    Signup,
+    Login,
+    Home,
+    Post,
+    EditPost,
+    AddPost,
+    AllPosts,
+}
